e280abb0ce6f43f165ad256e14bd96bcf9c074d0
hemanth05bb <hemanth0526>
2026-03-27 15:55:59 +0530

    uc1

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,31 @@
+/**
+ * ===============================================================
+ * MAIN CLASS - UseCase1HotelBookingApp
+ * ===============================================================
+ * * Use Case 1: Application Entry & Welcome Message
+ * * Description:
+ * This class represents the entry point of the
+ * Hotel Booking Management System.
+ * * @author Developer
+ * @version 1.0
+ */
+public class BookMyStayApp {
+
+    /**
+     * Application entry point.
+     * * This method is the first method executed
+     * when the program is launched by the JVM.
+     * * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        // Step 1: Display the welcome message
+        System.out.println("Welcome to the Hotel Booking Management System!");
+
+        // Step 2: Display Application Name and Version
+        System.out.println("Application Name: My Book App");
+        System.out.println("Version: 1.0");
+
+        // Step 3: Confirm successful startup
+        System.out.println("System started successfully.");
+    }
+}
